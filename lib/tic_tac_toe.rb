6b2255a36800f57259eaa92cc1d867a79c538c01
Peter Coughlin <coughlin1966@gmail.com>
2018-03-16 16:12:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,10 +32,10 @@ def move(index, value)
 end
 
 def position_taken?(index)
-  if @board[index] == " " || @board[index] == "" || @board[index] == nil
+  if @board[index] == " " || @board[index] == "" || @board[index] == nil &&
   #false
-  elsif @board[index] == "X" || @board[index] =="O"
-  #true
+  @board[index] == "X" || @board[index] =="O"
+  true
   end
 end
 
